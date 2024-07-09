83ab57ccd458b40e6761b48d8cb69ba4137336ba
Arbi Dwi Wijaya <arbi.nmaki919@gmail.com>
2024-07-09 09:07:34 +0700

    first

---
+++ src/test/java/com/spring/restful/api/RestfulApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.restful.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
